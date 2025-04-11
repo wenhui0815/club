fa2767d342a6e5475452a9e05c608869d1f1d0a0
wenhui0815 <maygohui0815@gmail.com>
2025-04-11 13:01:16 +0800

    【20250326】證書題2-2

---
+++ AtCoder-374.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double A, B;
+    cin >> A >> B;
+    double S = static_cast<double> (B/A);
+    cout << fixed << setprecision(3);
+    cout << S << endl;
+}
