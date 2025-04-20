f18c810083259b2d6a92baa3accfbe18d114c094
wenhui0815 <maygohui0815@gmail.com>
2025-04-20 23:00:43 +0800

    【20250417】證書題3-2

---
+++ AtCoder-319.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string st;
+    cin>>st;
+    if(st=="tourist")
+        cout<<3858<<endl;
+    else if(st=="ksun48")
+        cout<<3679<<endl;
+    else if(st=="Benq")
+        cout<<3658<<endl;
+    else if(st=="Um_nik")
+        cout<<3648<<endl;
+    else if(st=="apiad")
+        cout<<3638<<endl;
+    else if(st=="Stonefeang")
+        cout<<3630<<endl;
+    else if(st=="ecnerwala")
+        cout<<3613<<endl;
+    else if(st=="mnbvmar")
+        cout<<3555<<endl;
+    else if(st=="newbiedmy")
+        cout<<3516<<endl;
+    else if(st=="semiexp")
+        cout<<3481<<endl;
+    return 0;
+}
